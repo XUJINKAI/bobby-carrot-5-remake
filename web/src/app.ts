70846e4dd31be73603d77a5b2e10d9772ec3704e
XUJINKAI <XUJINKAI@users.noreply.github.com>
2026-08-26 14:14:47 +0800

    筛选样式

--- web/src/app.ts
+++ web/src/app.ts
@@ -1,6 +1,7 @@
 import { BobbyApp } from "./app/BobbyApp.js";
 import "../style.css";
 import "../game-ui.css";
+import "../level-filters.css";
 import "../../editor/style.css";
 
 const root = document.querySelector<HTMLDivElement>("#app");
