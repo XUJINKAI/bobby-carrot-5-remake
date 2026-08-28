77534f978b7a8265cddd0a7a6f1c9e486184498a
XUJINKAI <XUJINKAI@users.noreply.github.com>
2026-08-28 15:05:52 +0800

    refactor(model): rename egg objective entities

--- model/src/ids.ts
+++ model/src/ids.ts
@@ -46,8 +46,8 @@ export const EntityTypeId = {
 
   CONSUMED_CARROT: "consumed-carrot",
   CARROT: "carrot",
-  EGG_NEST_EMPTY: "egg-nest-empty",
-  EGG_NEST_FILLED: "egg-nest-filled",
+  EGG_EMPTY: "egg-empty",
+  EGG_FILLED: "egg-filled",
   LOCK: "lock",
   BEANSTALK_TIP: "beanstalk-tip",
   BEAN: "bean",
