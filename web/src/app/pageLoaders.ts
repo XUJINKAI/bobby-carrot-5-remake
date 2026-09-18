410c4adcc4214af202fbfce6bfa6dda10124b63c
XUJINKAI <XUJINKAI@users.noreply.github.com>
2026-09-18 21:23:07 +0800

    refactor(web): define pages and i18n scopes in one registry

--- web/src/app/pageLoaders.ts
+++ web/src/app/pageLoaders.ts
@@ -1,10 +1,57 @@
 import type { TranslationScope } from "@bobby/i18n";
 import { preloadWebI18nScopes } from "../i18n/webI18n.js";
 
+interface PageDefinition<T> {
+  readonly scopes: readonly TranslationScope[];
+  readonly importer: () => Promise<T>;
+}
+
 export interface LocalizedPageLoader<T> {
   (): Promise<T>;
   readonly scopes: readonly TranslationScope[];
 }
+
+function definePage<T>(
+  scopes: readonly TranslationScope[],
+  importer: () => Promise<T>,
+): PageDefinition<T> {
+  return { scopes, importer };
+}
+
+export const PAGE_REGISTRY = {
+  home: definePage(
+    ["home"],
+    () => import("../pages/home/mountHomePage.js"),
+  ),
+  explore: definePage(
+    ["explore"],
+    () => import("../pages/explore/mountExplorePage.js"),
+  ),
+  adventure: definePage(
+    ["adventure"],
+    () => import("../pages/adventure/mountAdventurePages.js"),
+  ),
+  game: definePage(
+    ["game"],
+    () => import("../pages/game/mountGamePage.js"),
+  ),
+  editor: definePage(
+    ["editor", "game"],
+    () => import("../pages/editor/mountEditorPage.js"),
+  ),
+  settings: definePage(
+    ["settings"],
+    () => import("../pages/settings/mountSettingsPage.js"),
+  ),
+  import: definePage(
+    ["import"],
+    () => import("../pages/import/mountImportPage.js"),
+  ),
+  embed: definePage(
+    ["embed"],
+    () => import("../pages/embed/mountEmbedPage.js"),
+  ),
+} as const;
 
 export function localizedPageScopes(
   ...loaders: LocalizedPageLoader<unknown>[]
@@ -13,55 +60,27 @@ export function localizedPageScopes(
 }
 
 function createLocalizedPageLoader<T>(
-  scopes: readonly TranslationScope[],
-  importer: () => Promise<T>,
+  definition: PageDefinition<T>,
 ): LocalizedPageLoader<T> {
   const load = async (): Promise<T> => {
     const [page] = await Promise.all([
-      importer(),
-      preloadWebI18nScopes(scopes),
+      definition.importer(),
+      preloadWebI18nScopes(definition.scopes),
     ]);
     return page;
   };
-  return Object.assign(load, { scopes });
+  return Object.assign(load, { scopes: definition.scopes });
 }
 
-export const loadHomePage = createLocalizedPageLoader(
-  ["home"],
-  () => import("../pages/home/mountHomePage.js"),
-);
-
-export const loadExplorePage = createLocalizedPageLoader(
-  ["explore"],
-  () => import("../pages/explore/mountExplorePage.js"),
-);
-
+export const loadHomePage = createLocalizedPageLoader(PAGE_REGISTRY.home);
+export const loadExplorePage = createLocalizedPageLoader(PAGE_REGISTRY.explore);
 export const loadAdventurePages = createLocalizedPageLoader(
-  ["adventure"],
-  () => import("../pages/adventure/mountAdventurePages.js"),
+  PAGE_REGISTRY.adventure,
 );
-
-export const loadGamePage = createLocalizedPageLoader(
-  ["game"],
-  () => import("../pages/game/mountGamePage.js"),
-);
-
-export const loadEditorPage = createLocalizedPageLoader(
-  ["editor", "game"],
-  () => import("../pages/editor/mountEditorPage.js"),
-);
-
+export const loadGamePage = createLocalizedPageLoader(PAGE_REGISTRY.game);
+export const loadEditorPage = createLocalizedPageLoader(PAGE_REGISTRY.editor);
 export const loadSettingsPage = createLocalizedPageLoader(
-  ["settings"],
-  () => import("../pages/settings/mountSettingsPage.js"),
+  PAGE_REGISTRY.settings,
 );
-
-export const loadImportPage = createLocalizedPageLoader(
-  ["import"],
-  () => import("../pages/import/mountImportPage.js"),
-);
-
-export const loadEmbedPage = createLocalizedPageLoader(
-  ["embed"],
-  () => import("../pages/embed/mountEmbedPage.js"),
-);
+export const loadImportPage = createLocalizedPageLoader(PAGE_REGISTRY.import);
+export const loadEmbedPage = createLocalizedPageLoader(PAGE_REGISTRY.embed);
