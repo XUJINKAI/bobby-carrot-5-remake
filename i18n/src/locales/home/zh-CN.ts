1bd788a1eae9c715c38f4ad6045d8e31203593ab
XUJINKAI <XUJINKAI@users.noreply.github.com>
2026-09-21 13:42:24 +0800

    调整首页导入入口文案

--- i18n/src/locales/home/zh-CN.ts
+++ i18n/src/locales/home/zh-CN.ts
@@ -9,7 +9,7 @@ const catalog = {
   "home.editor": "地图编辑器",
   "home.editorDescription": "编辑地图，通过链接或内嵌等方式分享",
   "home.import": "导入地图",
-  "home.importDescription": "导入自定义地图或存档",
+  "home.importDescription": "导入地图或存档数据",
   "home.embed": "将地图内嵌到其他网页",
   "home.importDialog": "导入数据",
   "home.importPlaceholder": "粘贴 JSON、Payload 或分享链接……",
